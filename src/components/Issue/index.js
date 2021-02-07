b93a3785ecc906c60d9b9d343a2b5030a9c0b7cd
Juan Garcia <juan.garcia@sonorasoft.com>
2021-02-07 02:25:17 -0700

    function to move around list items with keyboard arrow

--- src/components/Issue/index.js
+++ src/components/Issue/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { ListItem, Title, Anchor } from './styles'
 
-export const Issue = ({ title, url }) => {
+export const Issue = ({ title, url, handleOnClickIndex }) => {
   return (
-    <ListItem tabIndex='0'>
+    <ListItem tabIndex='0' onClick={handleOnClickIndex}>
       <Title> {title} </Title>
       <Anchor href={url}> {url} </Anchor>
     </ListItem>
--- src/components/ListOfIssues/index.js
+++ src/components/ListOfIssues/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import { useGetIssues } from '../../hooks/useGetIssues'
 import { Issue } from '../Issue'
 import { List } from './styles'
@@ -6,15 +6,34 @@ import { Loading } from '../Loading'
 import { IssueContext } from '../../context/IssuesContext'
 
 export const ListOfIssues = () => {
+  const [currentItem, setCurrentItem] = useState(0)
+  const listRef = useRef(null)
   const { loading } = useGetIssues()
   const { issues = [] } = useContext(IssueContext)
+
+  const handleOnKeyDown = (evt) => {
+    switch (evt.key) {
+      case 'ArrowDown':
+        setCurrentItem(currentItem + 1)
+        listRef.current.childNodes[currentItem + 1].focus()
+        break
+      case 'ArrowUp':
+        currentItem > 0 && setCurrentItem(currentItem - 1)
+        listRef.current.childNodes[currentItem - 1].focus()
+        break
+    }
+  }
+
+  const handleOnClickIndex = (index) => () => {
+    setCurrentItem(index)
+  }
 
   if (loading) return <Loading />
 
   return (
-    <List tabIndex='0'>
+    <List ref={listRef} onKeyDown={handleOnKeyDown}>
       {
-        issues.map(issue => <Issue key={issue.id} {...issue} />)
+        issues.map((issue, index) => <Issue key={issue.id} {...issue} handleOnClickIndex={handleOnClickIndex(index)} />)
       }
     </List>
   )
